26b4cc2219509219ff5148e9180552989c9d370b
Nicolas Valenzuela <nicolas.valenzuela@trebol-it.cl>
2021-10-29 15:44:14 -0300

    optimizacion de codigo

--- Assets/Scripts/PoolManager.cs
+++ Assets/Scripts/PoolManager.cs
@@ -6,12 +6,14 @@ public class PoolManager : MonoBehaviour
 {
     public int poolSize;
     public static PoolManager sharedInstance;
-    [SerializeField] public List<GameObject> lista;
-    [SerializeField] public List<GameObject> lista2;
+    [SerializeField] public List<GameObject> listPrefab;
+    [SerializeField] public List<GameObject> listPrefabClone;
     public List<GameObject> cube;
     public List<GameObject> cube2;
     public int contadorCube= 0;
     public int contadorCube2 = 0;
+    [SerializeField]
+    private Dictionary<string, int> users;
 
     void Awake()
     {
@@ -29,57 +31,83 @@ public class PoolManager : MonoBehaviour
     {
 
         for (int i = 0; i < poolSize; i++)
-        {
+        {   // objectList : []
+            for (int listPrefabItem = 0; listPrefabItem < listPrefab.Count; listPrefabItem++)
+            {
+                GameObject newObj = Instantiate(listPrefab[listPrefabItem]);
+                listPrefabClone.Add(newObj);
+                newObj.SetActive(false);
+                //objectList.Add(newObj)
 
-            GameObject newObj = Instantiate(lista[0]);
-            GameObject newObj2 = Instantiate(lista[1]);
-            lista2.Add(newObj);
-            lista2.Add(newObj2);
-            newObj.SetActive(false);
-            newObj2.SetActive(false);
-            cube = lista2.FindAll(e => e.name == "Cube(Clone)");
-            cube2 = lista2.FindAll(e => e.name == "Cube2(Clone)");
+            }
+            // dict : {cube2: [objectList] }
         }
+        /*string cadena = listPrefab[0].ToString();
+        char delimitador = '(';
+        string[] valores = cadena.Split(delimitador);
+        print("Valores " + valores[0]);
+        print(listPrefabClone[0]);
+        print(listPrefab[0]);*/
+        //cube = listPrefabClone.FindAll(e => e.name == "Cube(Clone)");
+        //cube2 = listPrefabClone.FindAll(e => e.name == "Cube2(Clone)");
     }
 
     public GameObject GetObjectFromPool(Vector3 newPosition, Quaternion newRotation, string num)
     {
+
+
         if (num == "1")
         {
-            
-            if (contadorCube<=cube.Count -1)
+            cube = listPrefabClone.FindAll(e => e.name == "Cube(Clone)");
+            //int contador1 = int.Parse(contador);
+            if(contadorCube <= cube.Count - 1)
             {
                 cube[contadorCube].SetActive(true);
                 contadorCube++;
-                return cube[contadorCube-1];
+                return cube[contadorCube - 1];
             }
-            else if (contadorCube > cube.Count-1)
+            else if (contadorCube > cube.Count - 1)
             {
                 contadorCube = 0;
                 cube[contadorCube].SetActive(true);
                 contadorCube++;
-                return cube[contadorCube-1];
+                return cube[contadorCube - 1];
             }
 
-            
+            /*if (contadorCube <= cube.Count - 1)
+            {
+                cube[contadorCube].SetActive(true);
+                contadorCube++;
+                return cube[contadorCube - 1];
+            }
+            else if (contadorCube > cube.Count - 1)
+            {
+                contadorCube = 0;
+                cube[contadorCube].SetActive(true);
+                contadorCube++;
+                return cube[contadorCube - 1];
+            }*/
+
+
 
         }
         else if (num == "2")
         {
-            
-            if (contadorCube2 <= cube2.Count -1)
+            cube = listPrefabClone.FindAll(e => e.name == "Cube2(Clone)");
+
+            if (contadorCube2 <= cube.Count -1)
             {
-                cube2[contadorCube2].SetActive(true);
+                cube[contadorCube2].SetActive(true);
                 contadorCube2++;
-                return cube2[contadorCube2-1];
+                return cube[contadorCube2-1];
                 
             }
-            else if(contadorCube2 > cube2.Count-1)
+            else if(contadorCube2 > cube.Count-1)
             {
                 contadorCube2 = 0;
-                cube2[contadorCube2].SetActive(true);
+                cube[contadorCube2].SetActive(true);
                 contadorCube2++;
-                return cube2[contadorCube2-1];
+                return cube[contadorCube2-1];
             }
 
         }
--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -7,15 +7,23 @@ public class Spawn : MonoBehaviour
     public Transform spawnPoint;
     public GameObject cubePrefab;
     public float cubeForce = 3.0f;
+    public int contador = 0;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject cube = PoolManager.sharedInstance.GetObjectFromPool(spawnPoint.position, spawnPoint.rotation,"1");
+            string contador1 = contador.ToString() ;
+            GameObject cube = PoolManager.sharedInstance.GetObjectFromPool(spawnPoint.position, spawnPoint.rotation, "1");
+            //Contador();
             Rigidbody rb = cube.GetComponent<Rigidbody>();
             rb.AddForce(spawnPoint.up * cubeForce, ForceMode.Impulse);
         } 
 
     }
+    /*public int Contador()
+    {
+        print("Contador " + contador);
+        return contador++;
+    }*/
 
 }
